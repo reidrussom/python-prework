9346da9aa2c23c3f990e90de18c3046b0538e8b1
Reid Russom <65356934+reidrussom@users.noreply.github.com>
2026-08-05 18:26:14 -0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-r = requests.get("https://countries.dev/countries")
-countries = r.json()
-
-name = input("Enter a country: ")
-
-for c in countries:
-    print(c["name"], c["population"], c["region"])
